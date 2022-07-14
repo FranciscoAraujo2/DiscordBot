7865a458f89c6e7a0dc0bb0e5b23a900c91a907f
Francisco <102423868+FranciscoAraujo2@users.noreply.github.com>
2022-07-14 12:06:07 +0100

    Update main.py

--- main.py
+++ main.py
@@ -11,4 +11,14 @@ async def on_ready():
     print("The bot is ready!")
     await bot.change_presence(status=disnake.Status.idle, activity=game)
    
+extensions = []
+directory = 'cogs'
+for file in os.listdir(directory):
+    if file.endswith('.py'):
+        extensions.append(f'{directory}.{file[:-3]}')
+
+for extension in extensions:
+    bot.load_extension(extension)
+    print(f'{extension} loaded!')
+   
 bot.run(token)
